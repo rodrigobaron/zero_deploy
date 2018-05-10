4a29855c316ca4ec9fa40cbc797ec07b6f5c1676
Rodrigo Baron <baron.rodrigo0@gmail.com>
2018-05-10 12:05:02 -0300

    make pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='zero_deploy',
+    version='0.1.0',
+    description='Manange enviroment wich the code will be executed',
+    author='Rodrigo Baron <baron.rodrigo0@gmail.com>',
+    url='https://github.com/rodrigobaron/zero_deploy',
+    packages=['zero_deploy'],
+)
