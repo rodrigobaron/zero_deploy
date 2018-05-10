c5df5a815d0b198ad68af994225530e7526935d7
Rodrigo Baron <baron.rodrigo0@gmail.com>
2018-05-10 12:04:42 -0300

    minimal documentation

--- zero_deploy/env.py
+++ zero_deploy/env.py
@@ -1,3 +1,7 @@
+"""
+Enviroment is the core of zero_deploy package. It control when and how modules will be used, loading configuration
+and local machine enviroment variable.
+"""
 from plumbum import SshMachine
 import rpyc
 from rpyc.utils.zerodeploy import DeployedServer
@@ -9,6 +13,8 @@ import os.path as P
 import sys
 
 class LocalMachine:
+    """ Mock remote machine as local machine. Returns 'LocalEnv' when a connection are made.
+    """
     def classic_connect(self):
         return LocalEnv()
     
@@ -16,12 +22,25 @@ class LocalMachine:
         pass
 
 class LocalEnv:
+    """ Use local machine as fallback if no configurations are found.
+    """
     def __init__(self):
         self.modules = ModuleNamespace(Slave().getmodule)
         self.builtin = self.modules.builtins
 
 class Env(object):
+    """ Enviroment mananger will control the actual enviroment at runtime. It load `~/.zero_deploy.yaml` configuration
+    if avaliable and parse the list of configurations. Alos allow use enviroment variable `ZERO_DEPLOY_SERVERS` to
+    choose which server's the connection will be made.
+    """
     def __init__(self, servers=None, use_env=False):
+        """ If no servers or enviroment variable are found the local machine will be the default Env. 
+        Params:
+        - servers: can be one or multiple `host` avaliable on configuration file. If more than one is provided then
+        multiples connections when call `connect`.
+        - use_env: if True will try use `ZERO_DEPLOY_SERVERS` local enviroment variable, if that enviroment variable are
+        not avaliable will fallback to `servers` params.
+        """
         if use_env and 'ZERO_DEPLOY_SERVERS' in os.environ:
             _servers = os.environ['ZERO_DEPLOY_SERVERS']
             if len(_servers) > 0:
@@ -101,11 +120,17 @@ class Env(object):
         return connections if len(connections) > 1 else connections[0]
 
 def remote_print(conn, *args):
+    """
+    Use remote and local sys.stdout.
+    """
     _print = "%s\n" % ( " ".join([str(p) for p in args]) )
     conn.modules.sys.stdout.write(_print)
     conn.modules.sys.stdout.flush()
 
 def remote_import(conn, modules, package=None):
+    """
+    Import remote module, if remote don't have the module the local module will be uploaded safely to remote.
+    """
     if type(modules) is not dict and type(modules) is not list:
         modules = [modules]
     
